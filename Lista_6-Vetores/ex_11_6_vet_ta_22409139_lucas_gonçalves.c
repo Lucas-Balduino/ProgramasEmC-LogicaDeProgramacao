8e16999e2c6498525415371ad8e49b0eb7d85713
Lucas-Balduino <165683227+Lucas-Balduino@users.noreply.github.com>
2024-05-07 11:26:37 -0300

    Update ex_11_6_vet_ta_22409139_lucas_gonçalves.c

--- Lista_6-Vetores/ex_11_6_vet_ta_22409139_lucas_gonçalves.c
+++ Lista_6-Vetores/ex_11_6_vet_ta_22409139_lucas_gonçalves.c
@@ -4,27 +4,29 @@
 
 //Exercicio 11
 
-/*Elabore um programa para limitar a quantidade m·xima de caracteres que poderemos
-digitar para uma entrada alfanumÈrica, ou seja, vari·vel string. O usu·rio fornecer·
-uma palavra ou uma sentenÁa que ser· finalizada quando ele digitar a tecla < enter >
-ou quando chegar ao limite m·ximo de sessenta caracteres digitados. No final mostre
+/*Elabore um programa para limitar a quantidade m√°xima de caracteres que poderemos
+digitar para uma entrada alfanum√©rica, ou seja, vari√°vel string. O usu√°rio fornecer√°
+uma palavra ou uma senten√ßa que ser√° finalizada quando ele digitar a tecla < enter >
+ou quando chegar ao limite m√°ximo de sessenta caracteres digitados. No final mostre
 a quantidade de caracteres efetivamente digitados.*/
 
 void main(){
 	setlocale (LC_ALL,"Portuguese");
 	int i;
 	char palavra[60];
-	
+
 	printf("Digite frase de ate 60 caracteres\n");
-	gets(palavra);
-	
-	i = strlen(palavra);
-	
-	if (i > 60){
-		printf("Limite de caracteres atingido\n");
-		printf("60 caracteres digitados");
-	}
-	else{
-		printf("%d caracteres digitados", i);
+
+	for (i = 0; i < 60; i++){
+        palavra[i] = getche();
+        if (palavra[i] == 13){
+            printf("Quantidade de caracteres digitados: %d", i);
+            break;
+        }
+        if (i == 60){
+            printf("Limite de caracteres atingido!");
+            printf("60 caracteres digitados");
+        }
 	}
 }
+
