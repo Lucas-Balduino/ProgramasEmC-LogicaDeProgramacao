4cd8d5c7fa807289ffc6c82348c993006eb58bff
Lucas-Balduino <165683227+Lucas-Balduino@users.noreply.github.com>
2024-05-07 11:05:10 -0300

    Update ex_09_6_vet_ta_22409139_lucas_gonçalves.c

--- Lista_6-Vetores/ex_09_6_vet_ta_22409139_lucas_gonçalves.c
+++ Lista_6-Vetores/ex_09_6_vet_ta_22409139_lucas_gonçalves.c
@@ -4,10 +4,10 @@
 
 //Exercicio 9
 
-/*. Elabore um programa que, dados dois vetores inteiros de 20 posições, efetue as
-respectivas operações indicadas por um outro vetor de 20 posições de caracteres
-também fornecido pelo usuário, contendo as quatro operações aritméticas em
-qualquer combinação, armazenando os resultados num quarto vetor.*/
+/*. Elabore um programa que, dados dois vetores inteiros de 20 posiÃ§Ãµes, efetue as
+respectivas operaÃ§Ãµes indicadas por um outro vetor de 20 posiÃ§Ãµes de caracteres
+tambÃ©m fornecido pelo usuÃ¡rio, contendo as quatro operaÃ§Ãµes aritmÃ©ticas em
+qualquer combinaÃ§Ã£o, armazenando os resultados num quarto vetor.*/
 
 void main(){
 	setlocale (LC_ALL,"Portuguese");
@@ -16,22 +16,22 @@ void main(){
 	int valores2 [20];
 	char operadores [20];
 	float resultados [20];
-	
+
 	printf("Digite 20 valores\n");
 	for (i = 0; i < 20; i++){
 		scanf("%d", &valores1[i]);
 	}
-	
+
 	printf("Digite mais 20 valores\n");
 	for (i = 0; i < 20; i++){
 		scanf("%d", &valores2[i]);
 	}
-	
+
 	printf("Digite 20 operadores aritmeticos\n");
 	for (i = 0; i < 20; i++){
-		scanf("%c", &operadores[i]);
+		scanf(" %c", &operadores[i]);
 	}
-	
+
 	for (i = 0; i < 20; i++){
 		if (operadores[i] == '+'){
 			resultados[i] = (float)valores1[i] + (float)valores2[i];
@@ -45,9 +45,9 @@ void main(){
 		if (operadores[i] == '*'){
 			resultados[i] = (float)valores1[i] * (float)valores2[i];
 		}
-		
+
 	}
-	
+
 	for (i = 0; i < 20; i++){
 		printf("%d %c %d = %.2f\n",valores1[i], operadores[i], valores2[i], resultados[i]);
 	}
