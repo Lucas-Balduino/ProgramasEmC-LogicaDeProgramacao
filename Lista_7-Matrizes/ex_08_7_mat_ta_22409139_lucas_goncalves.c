710656165e90e7209728be2ce4649a9b3d4f4685
Lucas-Balduino <165683227+Lucas-Balduino@users.noreply.github.com>
2024-05-24 10:34:25 -0300

    Exercicios Lista 7

--- Lista_7-Matrizes/ex_08_7_mat_ta_22409139_lucas_goncalves.c
+++ Lista_7-Matrizes/ex_08_7_mat_ta_22409139_lucas_goncalves.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
+#include <conio.h>
 
 //Exercicio 8
 
@@ -13,28 +14,61 @@ b) Conte o total de caracteres convertidos para maiúsculos e apresente o valor n
 
 void main (){
     setlocale (LC_ALL,"Portuguese");
-    int matriz1 [5][5];
-    int i, j;
+    char matriz [25][5];
+    int i, j, linhaMaria = -1, contador = 0, contadorMaiusculas = 0;
     
-    printf("Digite 25 nomes \n");
+    printf("Digite 25 nomes\n");
     
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 25; i++)
     {
         for (j = 0; j < 5; j++)
         {
-            scanf("%d", &matriz1[i][j]);
+            matriz[i][j] = getche();
+            if (matriz[i][j] >= 97 && matriz[i][j] <= 122){
+            	matriz[i][j] -= 32;
+            	contadorMaiusculas++;
+			}
+            
+            if(matriz[i][j]=='M' && j == 0){
+            	contador++;
+			}
+			if(matriz[i][j]=='A'&& j == 1){
+            	contador++;
+			}
+			if(matriz[i][j]=='R'&& j == 2){
+            	contador++;
+			}
+			if(matriz[i][j]=='I'&& j == 3){
+            	contador++;
+			}
+			if(matriz[i][j]=='A'&& j == 4){
+            	contador++;
+			}
+			if (contador == 5){
+				linhaMaria = i;
+			}
         }
+        contador = 0;
+        printf("\n");
     }
     
     printf("A matriz com os nomes fica:\n");
             
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 25; i++)
     {
+    	printf("[");
         for (j = 0; j < 5; j++)
         {
-            printf("[%d]    ", resultado[i][j]);
+            printf("%c", matriz[i][j]);
         }
-       printf("\n");
+       printf("]\n");
     }
+    if(linhaMaria != -1){
+    	printf("O nome Maria foi digitado na linha %d\n", linhaMaria);	
+	}
+	else{
+		printf("O nome Maria não foi encontrado\n");	
+	}
+    printf("%d caracteres foram convertidos para maiusculas", contadorMaiusculas);
     
 }
